3da889b9b04d226a8346c51b380d2d404b22bd48
Sivert Lysnes <vuvulala14@gmail.com>
2024-01-26 10:04:50 +0100

    with this

---
+++ src/lib/components/sectionWrapper.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let title: string = "Untitled"
+</script>
+
+<div class="top">
+    <h1>{title}</h1>
+    <slot/>
+</div>
+
+<style>
+    .top h1 {
+        display: flex;
+        align-self: center;
+    }
+    .top {
+        display: flex;
+        flex-direction: column;
+        border-style: solid;
+        height: 100%;
+    }
+</style>
